92c90c802c6be229ffeba0a2baf74db997585462
Shubham Singh <shubhamsingh7167424@gmail.com>
2024-08-13 19:37:53 +0530

    area of rectangle

---
+++ Logic Building/2.area-of-rectangle.js
@@ -0,0 +1,16 @@
+// Q. Wap to find the area of rectangle
+
+function areaOfRectangle(l, b) {
+      return l*b;
+}
+
+console.log(areaOfRectangle(5,3));
+console.log(areaOfRectangle(38,3));
+
+
+const areaOfRectangle2 = (l, b) =>  l*b;
+
+console.log(areaOfRectangle2(48, 2));
+console.log(areaOfRectangle2(928, 4));
+
+
