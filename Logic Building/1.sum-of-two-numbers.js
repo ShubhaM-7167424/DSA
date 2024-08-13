aab0e3d671b5929464895031adc2a8b27fb3ecb2
Shubham Singh <shubhamsingh7167424@gmail.com>
2024-08-13 19:36:21 +0530

    sum of two numbers

---
+++ Logic Building/1.sum-of-two-numbers.js
@@ -0,0 +1,15 @@
+// Q. Wap to add two numbers
+
+function sum(a,b) {
+      return a+b;
+}
+
+console.log(sum(2, 3));
+console.log(sum(48, 293));
+
+
+const sum2 = (a, b) => a+b;
+
+console.log(sum2(35,23));
+console.log(sum2(483, 3892));
+
