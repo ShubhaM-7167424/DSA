359488559c95a8f6394b7acc28307f0c120c72ee
Shubham Singh <shubhamsingh7167424@gmail.com>
2024-09-12 10:45:18 +0530

    smallest of three numbers

---
+++ Logic Building/4.smallest-of-three-numbers.js
@@ -0,0 +1,15 @@
+// Q. Wap to find the smalles of the three numbers
+
+function smallestOfThreeNumbers(a, b, c) {
+    if (a < b && a < c) {
+        return a;
+    } else if (b < a && b < c) {
+        return b;
+    } else {
+        return c;
+    }
+}
+
+console.log(smallestOfThreeNumbers(1, 2, 3));
+console.log(smallestOfThreeNumbers(23, 12, 3));
+console.log(smallestOfThreeNumbers(40, 12, 33));
