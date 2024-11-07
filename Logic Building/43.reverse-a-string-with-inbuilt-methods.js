3b72fcc81322671e8d596b52a9d93cea2aa39859
Shubham Singh <shubhamsingh7167424@gmail.com>
2024-11-07 20:09:21 +0530

    reverse a string

---
+++ Logic Building/43.reverse-a-string-with-inbuilt-methods.js
@@ -0,0 +1,9 @@
+// Q. Wap to reverse a string using inbuilt methods
+
+function reverseAString(str) {
+       
+       return str.split('').reverse().join('') 
+        
+}
+
+console.log(reverseAString('love'));
