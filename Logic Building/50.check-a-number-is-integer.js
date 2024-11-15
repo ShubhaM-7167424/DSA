2a1e74ab39e2243e876592e55104362220e27959
Shubham Singh <shubhamsingh7167424@gmail.com>
2024-11-15 21:55:15 +0530

    check a number is integer

---
+++ Logic Building/50.check-a-number-is-integer.js
@@ -0,0 +1,13 @@
+// Q. How to check if a number is an integer ?
+//    Do not use Number.isInteger(4.3)
+
+
+let a = 11.0
+
+if(a%1 === 0){
+        console.log('integer');
+        
+}else{
+        console.log('!not');
+        
+}
