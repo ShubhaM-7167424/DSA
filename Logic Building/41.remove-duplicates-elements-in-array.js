bb7fcd0d203cf88f4be8f4d04f772301cc8e6ce9
Shubham Singh <shubhamsingh7167424@gmail.com>
2024-12-03 17:58:12 +0530

    remove duplicates from array

--- Logic Building/41.remove-duplicates-elements-in-array.js
+++ Logic Building/41.remove-duplicates-elements-in-array.js
@@ -1,24 +1,17 @@
 // Q. Wap to remove the duplicates element in an array
 
 function removeDuplicates(arr) {
-  return arr.filter((value, index) => {
-                console.log(value);
-                console.log(index);
-   console.log(arr.indexOf(value));
-   
-   return arr.indexOf(value) === index
-        
-                
-  });
+    return arr.filter((value, index) => {
+        console.log(value, index, arr.indexOf(value));
+
+        return arr.indexOf(value) === index;
+    });
 }
 
-
-console.log(removeDuplicates([1,2, 3, 2, 4, 5, 2]));
+console.log(removeDuplicates([1, 2, 3, 2, 4, 5, 2]));
 console.log(removeDuplicates([23, 43, 45, 23, 50, 43]));
 
-
 //? ******************************************************************
-
 
 let arr = [11, 23, 45, 11, 45, 11, 45, 76, 45, 83, 11, 91];
 
@@ -33,8 +26,3 @@ function removeDuplicates(arr) {
 }
 
 console.log(removeDuplicates(arr));
-
-
-
-
-
