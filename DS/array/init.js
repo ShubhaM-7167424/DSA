e9708fe08489c20dbae7493b41f56415900baf43
Shubham Singh <shubhamsingh7167424@gmail.com>
2025-01-01 20:06:45 +0530

    creating array using square brackets

---
+++ DS/array/init.js
@@ -0,0 +1,22 @@
+let myName = 'Shubham'
+console.log(myName);
+
+let myNames = ['Shubham', 'Raj', 'Rahul', 'Rohan']
+console.log(myNames);
+
+let mathScore = 89
+console.log(mathScore);
+
+let mathScores = [89, 78, 67, 56]
+console.log(mathScores);
+
+let firstStudent = {name: 'Shubham', city: 'Delhi', country: 'India'}
+console.log(firstStudent);
+
+let students = [
+    {name: 'Shubham', city: 'Delhi', country: 'India'},
+    {name: 'Raj', city: 'Mumbai', country: 'India'},
+    {name: 'Rahul', city: 'Kolkata', country: 'India'},
+    {name: 'Rohan', city: 'Chennai', country: 'India'}
+]
+console.log(students);
